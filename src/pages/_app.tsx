97a5d85c9ce52e3aff3073d37f82be231fc9931a
yyueniao <yyueniao0913@gmail.com>
2023-04-15 15:36:26 +0800

    feat: init the content
    Prior to this commit, the page is still the default layout of Next.js.
    I will start updating the ui from this commit.
    
    In this commit, I updated the title and meta description, and
    temporarily filled the web page with some texts generated by chatGPT,
    this will be updated in future commits.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Ng Say Peng</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,15 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document(): JSX.Element {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Welcome to my online resume. I'm a software developer experienced in frontend and backend development."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
