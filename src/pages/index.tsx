03806d7c499198e8ed5832c8b9eb79920eb60277
yyueniao <yyueniao0913@gmail.com>
2023-04-16 09:53:13 +0800

    feat: add `EducationSection`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import AboutMeSection from '@/components/sections/aboutMe/AboutMeSection';
+import EducationSection from '@/components/sections/education/EducationSection';
 import ExperienceSection from '@/components/sections/experience/ExperienceSection';
 import { Stack } from '@mui/material';
 
@@ -7,6 +8,7 @@ export default function Home(): JSX.Element {
     <Stack>
       <AboutMeSection />
       <ExperienceSection />
+      <EducationSection />
     </Stack>
   );
 }
