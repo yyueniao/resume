e5d1e0cc3e78941555ddfb75b4666067798b4fc3
yyueniao <yyueniao0913@gmail.com>
2023-04-16 09:46:49 +0800

    feat: add `ExperienceSection`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import AboutMeSection from '@/components/sections/aboutMe/AboutMeSection';
+import ExperienceSection from '@/components/sections/experience/ExperienceSection';
 import { Stack } from '@mui/material';
 
 export default function Home(): JSX.Element {
   return (
     <Stack>
       <AboutMeSection />
+      <ExperienceSection />
     </Stack>
   );
 }
