3f156fbb2cf2e723504514ee4364aaa2f6630671
yyueniao <yyueniao0913@gmail.com>
2023-04-16 12:22:55 +0800

    style(index): add divider between sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,11 @@ import EducationSection from '@/components/sections/education/EducationSection';
 import ExperienceSection from '@/components/sections/experience/ExperienceSection';
 import ProjectSection from '@/components/sections/project/ProjectSection';
 import SkillSection from '@/components/sections/skill/SkillSection';
-import { Stack } from '@mui/material';
+import { Divider, Stack } from '@mui/material';
 
 export default function Home(): JSX.Element {
   return (
-    <Stack alignItems="center">
+    <Stack alignItems="center" spacing={10} divider={<Divider flexItem />}>
       <AboutMeSection />
       <ExperienceSection />
       <EducationSection />
