854e11c9004d556e582e64fe9066edcb996042b2
yyueniao <yyueniao0913@gmail.com>
2023-06-07 20:55:35 +0800

    style(index): responsive design
    Responsive design mainly for navigation bar.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,12 @@ import EducationSection from '@/components/sections/education/EducationSection';
 import ExperienceSection from '@/components/sections/experience/ExperienceSection';
 import ProjectSection from '@/components/sections/project/ProjectSection';
 import SkillSection from '@/components/sections/skill/SkillSection';
-import { Divider, Grid, Stack } from '@mui/material';
+import { Theme } from '@mui/material/styles';
+import { Divider, Grid, Stack, useMediaQuery } from '@mui/material';
 import { useRef } from 'react';
 
 export default function Home(): JSX.Element {
+  const isAboveMd = useMediaQuery<Theme>((theme) => theme.breakpoints.up('md'));
   const aboutMeRef = useRef(null);
   const experienceRef = useRef(null);
   const educationRef = useRef(null);
@@ -15,28 +17,31 @@ export default function Home(): JSX.Element {
   const skillRef = useRef(null);
 
   return (
-    <Grid container>
-      <Grid
-        item
-        xs={2}
-        sx={{
-          bgcolor: (theme) => theme.palette.primary.main,
-          color: (theme) => theme.palette.text.primary,
-          display: 'flex',
-          placeContent: 'center',
-        }}
-      >
-        <NavigationBar
-          sectionRefs={[
-            { ref: aboutMeRef, label: 'About Me' },
-            { ref: experienceRef, label: 'Experience' },
-            { ref: educationRef, label: 'Education' },
-            { ref: projectRef, label: 'Project' },
-            { ref: skillRef, label: 'Skill' },
-          ]}
-        />
-      </Grid>
-      <Grid item xs>
+    <Grid container columns={24}>
+      {isAboveMd && (
+        <Grid
+          item
+          md={4}
+          lg={3}
+          sx={{
+            bgcolor: (theme) => theme.palette.primary.main,
+            color: (theme) => theme.palette.text.primary,
+            display: 'flex',
+            placeContent: 'center',
+          }}
+        >
+          <NavigationBar
+            sectionRefs={[
+              { ref: aboutMeRef, label: 'About Me' },
+              { ref: experienceRef, label: 'Experience' },
+              { ref: educationRef, label: 'Education' },
+              { ref: projectRef, label: 'Project' },
+              { ref: skillRef, label: 'Skill' },
+            ]}
+          />
+        </Grid>
+      )}
+      <Grid item xs={12} md lg>
         <Stack alignItems="center" spacing={10} divider={<Divider flexItem />} sx={{ p: 2 }}>
           <AboutMeSection ref={aboutMeRef} />
           <ExperienceSection ref={experienceRef} />
