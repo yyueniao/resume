d055c826506f1f1f334519edb590b7858b1da45b
yyueniao <91043129+yyueniao@users.noreply.github.com>
2023-06-07 20:20:04 +0800

    style: add theme (#11)
    Use Mui `ThemeProvider` to set the primary and secondary color.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import '@/styles/globals.css';
+import { ThemeProvider, createTheme } from '@mui/material';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#3f51b5',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+    },
+  });
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Ng Say Peng</title>
@@ -12,6 +24,6 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
       <main>
         <Component {...pageProps} />
       </main>
-    </>
+    </ThemeProvider>
   );
 }
