f7fbb9cac272be39923f2e52ab7a1f84e855ec41
yyueniao <91043129+yyueniao@users.noreply.github.com>
2023-06-08 22:27:08 +0800

    feat(ProjectCard): design and utilize for "Resume" (#15)
    Implement `ProjectCard` component and utilize it for resume project.

--- src/components/sections/project/ProjectSection.tsx
+++ src/components/sections/project/ProjectSection.tsx
@@ -1,19 +1,22 @@
 import { Card, CardContent, Stack, Typography } from '@mui/material';
 import Link from 'next/link';
 import { forwardRef } from 'react';
+import ProjectCard from './ProjectCard';
 
 const ProjectSection = forwardRef<HTMLElement>(function ProjectSection(_props, ref) {
   return (
     <Stack component="section" alignItems="center" ref={ref}>
       <Typography variant="h3">PROJECTS</Typography>
       <Stack spacing={2}>
-        <Card sx={{ border: '1px solid black' }}>
-          <CardContent>
-            <Typography variant="h6">Resume</Typography>
-            <Link href="https://github.com/yyueniao/resume">Github Link</Link>
-            <Typography variant="body2">Yes, you are currently watching this project!</Typography>
-          </CardContent>
-        </Card>
+        <ProjectCard
+          title={'Resume'}
+          image={'/resumeThumbnail.png'}
+          description={
+            'Yes, you are currently looking at this project! It is written by React, TypeScript and Next.js.'
+          }
+          githubLink={'https://github.com/yyueniao/resume'}
+          websiteLink={'https://online-resume-sepia.vercel.app/'}
+        />
         <Card sx={{ border: '1px solid black' }}>
           <CardContent>
             <Typography variant="h6">Calculator</Typography>
